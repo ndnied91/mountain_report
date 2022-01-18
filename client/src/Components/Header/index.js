22c36eed8a8503c67130ebba46db002c9bc4a286
Danny Niedzwiedzki <danielniedzwiedzki.1@gmail.com>
2022-01-18 16:15:07 -0500

    update

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -14,10 +14,16 @@ import './style.css'
 class Header extends React.Component{
   constructor(props){
     super(props)
-      this.state = { searchTerm: '' ,  open: false };
+      this.state = { searchTerm: '' ,  open: false  , notShow: true  };
       this.onInputchange = this.onInputchange.bind(this);
 
+      this.handleNavCollapse = () => {
+                if(this.state.notShow === true){ this.setState({notShow : false}) }
+                else{ this.setState({notShow : true}) }
+      }
+
   }
+
 
 
 
@@ -53,15 +59,55 @@ class Header extends React.Component{
 
       const renderLogin = () =>{
         return(
-          <div>
-              <div className="box">
-                  <div className="left"> <MoutainSelection/> </div>
-                  <div className="buttonUpdate">
-                      <div className="push"><SignUp title={'Sign Up'}/></div>
-                      <div className="push"><SignIn title={'Sign In'}/></div>
-                  </div>
-              </div>
-          </div>
+          // <div>
+          //     <div className="box">
+          //         <div className="left"> <MoutainSelection/> </div>
+          //         <div className="buttonUpdate">
+          //
+          //           <div ref={(ref) => this._div = ref} > {/* this is for dropdown, DONT TOUCH  */}
+          //               <div ref={node => this.node = node}> {/* this is for dropdown, DONT TOUCH  */}
+          //
+          //               <button className="navbar-toggler"  style={{float: 'right' }} type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          //                 <span onClick ={ ()=> this.handleNavCollapse() } className="navbar-toggler-icon"></span>
+          //               </button>
+          //
+          //                 <nav className="navbar navbar-expand-lg navbar-light bannerGround" >
+          //                     <div className={`${this.state.notShow ? 'collapse ' : null} navbar-collapse `} >
+          //                         <div className="push"><SignUp title={'Sign Up'}/></div>
+          //                         <div className="push"><SignIn title={'Sign In'}/></div>
+          //                     </div>
+          //                 </nav>
+          //             </div>
+          //          </div>
+          //
+          //         </div>
+          //     </div>
+          // </div>
+          <div ref={node => this.node = node}> {/* this is for dropdown, DONT TOUCH  */}
+
+                 <nav className="navbar navbar-expand-lg navbar-light bannerGround" >
+
+
+                    <div className="left"> <MoutainSelection/> </div>
+
+                       <button className="navbar-toggler"  style={{float: 'right' }} type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                         <span onClick ={ ()=> this.handleNavCollapse() } className="navbar-toggler-icon"></span>
+                       </button>
+
+                       <div className={`${this.state.notShow ? 'collapse ' : null} navbar-collapse `} >
+
+                      <div style= {{width: '100%' , height: '40px' }}>
+
+                        <ul className="navbar-nav mt-0 mt-lg-0 collapseStyling" >
+                          <li className="nav-item"> <div className="push"><SignUp title={'Sign Up'}/></div> </li>
+                          <li className="nav-item"> <div className="push"><SignIn title={'Sign In'}/></div> </li>
+                        </ul>
+
+                       </div>
+                     </div>
+                   </nav>
+
+             </div>
 
 
         )
